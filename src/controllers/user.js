83e9c7080e96d09e19456640f3b10c7c0699a99a
Georgii Galechyan <galechyan1991@gmail.com>
2023-08-14 00:24:27 +0400

    1) Решена проблема с ошибкой avvio. 2) Нужно решить проблему с ENV, он не работет. Возможно я не правильно подключилрегистрирую плагины.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,39 @@
+import { app } from '../../server.js'
+
 export default {
   createUser: async (request, reply) => {
     try {
-    } catch (error) {
-      app.log.error(error)
+      const { login, password, email } = request.body // получили наши данные
+      await createNewUser(login, password, email)
+
+      async function createNewUser(login, password, email) {
+        try {
+          await app.pg.transact(async (client) => {
+            await client.query(
+              `INSERT INTO users (login, password, email) VALUES ('${login}', '${password}', '${email}') RETURNING *;`
+            )
+            await request.log.info({ msg: `Пользователь '${login}' добавлен в таблицу 'users'` })
+          })
+        } catch (err) {
+          request.log.error(err)
+
+          // request.log.error({
+          //   msg: `Error adding user '${login}' to table 'users'`,
+          //   'Error code': err.code,
+          //   'Error type': err.type,
+          //   Message: err.message,
+          //   Detail: err.detail,
+          //   'DB table': err.table,
+          //   'DB constraint': err.constraint,
+          //   Stack: err.stack,
+          // })
+        }
+      }
+
+      app.log.info({ msg: 'Post метод отработал (logger)' })
+      await reply.send('Post метод отработал (reply.send)')
+    } catch (err) {
+      app.log.error({ msg: `Что-то пошло не так при регистрации пользователя ${request.body.login}` })
     }
   },
 
