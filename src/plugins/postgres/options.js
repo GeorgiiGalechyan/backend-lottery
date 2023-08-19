9ceea3bc4b9f8567e202157f73332abc5a42142b
Georgii Galechyan <galechyan1991@gmail.com>
2023-08-19 14:43:53 +0400

    EN: There is still a problem with reading .env variables when working with Postgres. RU: Осталась проблема с чтением переменных .env при работе с Postgres.

--- src/plugins/postgres/options.js
+++ src/plugins/postgres/options.js
@@ -1,9 +1,9 @@
 import pg from './pool.js'
 
-// export default {
-//   connectionString: `postgres://${pg.user}:${pg.password}@${pg.host}:${pg.port}/${pg.database}`,
-// }
-
 export default {
-  connectionString: 'postgres://nrdfntug:5cTPkbiPU_GlFB6sHTAaQQIPjNwZ9LCC@mouse.db.elephantsql.com:5432/nrdfntug',
+  connectionString: `postgres://${pg.user}:${pg.password}@${pg.host}:${pg.port}/${pg.database}`,
 }
+
+// export default {
+//   connectionString: 'postgres://nrdfntug:5cTPkbiPU_GlFB6sHTAaQQIPjNwZ9LCC@mouse.db.elephantsql.com:5432/nrdfntug',
+// }
